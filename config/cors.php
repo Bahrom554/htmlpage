0ca3b2057a0444d0e4905725359f3e955a3d3efe
Bahrom554 <bahrombozorboev554@gmail.com>
2023-02-03 20:31:10 +0500

    cors

--- config/cors.php
+++ config/cors.php
@@ -15,19 +15,26 @@ return [
     |
     */
 
+    
+     
+    
     'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => ['*'],
 
+    'Access-Control-Allow-Origin'=> '*',
+
+    'Access-Control-Allow-Headers'=> 'Origin, Content-Type',
+
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => false,
 
-    'max_age' => 0,
+    'max_age' => false,
 
     'supports_credentials' => false,
 
