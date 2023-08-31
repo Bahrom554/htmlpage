f221e9f030d60975cb70be4446fc8272cfca8639
Bahrom554 <bahrombozorboev554@gmail.com>
2023-08-31 10:19:38 +0500

    migrationchanged

--- database/migrations/2023_08_24_092126_create_manufacture_tool_type_table.php
+++ database/migrations/2023_08_24_092126_create_manufacture_tool_type_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateManufactureToolTable extends Migration
+class CreateManufactureToolTypeTable extends Migration
 {
     /**
      * Run the migrations.
