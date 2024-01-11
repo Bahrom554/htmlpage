a746eb7d05602f0cf748e8e627e0647a95d8d197
Bahrom554 <bahrombozorboev554@gmail.com>
2024-01-11 16:13:35 +0500

    asdsd

---
+++ database/migrations/2024_01_11_105426_add_new_column_applications.php
@@ -0,0 +1,42 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNewColumnApplications extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->string('mai_task')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->dropColumn('mai_task');
+        });
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
